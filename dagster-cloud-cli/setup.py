159a4c06c72b554807a63d5151890f96dbe356df
elementl <devtools@elementl.com>
2024-06-28 19:10:52 +0000

    Release 1.7.12 (tracks 4f2133b1367c2f195e33a4b5d1255281f39085b5)

--- dagster-cloud-cli/setup.py
+++ dagster-cloud-cli/setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(exclude=["dagster_cloud.cli_tests*"]),
     include_package_data=True,
     install_requires=[
-        "dagster==1.7.11",
+        "dagster==1.7.12",
         "packaging>=20.9",
         "questionary",
         "requests",
